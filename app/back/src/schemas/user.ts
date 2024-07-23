70d5645259b4a0dbd976ced1d843893f0b4800e1
vinimostaco <vini.mostaco@hotmail.com>
2024-07-23 13:36:55 -0300

    fix: fix folder structure

--- app/back/src/schemas/user.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-const createUserSchema = z.object({
-  name: z.string(),
-  email: z.string().email(),
-  password: z.string(),
-});
-
-export { createUserSchema };
--- app/back/src/server.ts
+++
@@ -1,4 +0,0 @@
-import { app } from "./app";
-import { env } from "./env";
-
-app.listen(env.PORT);
